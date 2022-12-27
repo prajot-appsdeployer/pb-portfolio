0940130a65093562f987820167fdb9d878b986f0
prajotb <120003461+prajot-appsdeployer@users.noreply.github.com>
2022-12-27 15:13:39 +0000

    make some subtle UI changes and some overall improvements

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-// too add Shadow on scroll
+// on scroll effects
+const mediaQuery = window.matchMedia("(max-width: 990px)");
+if (mediaQuery.matches) {
+  window.onload = function () {
+    navbar.classList.add("navbar-dark", "nav-color");
+  };
+
+  const navLinks = document.querySelectorAll(".nav-item");
+  const menuToggle = document.getElementById("navbarNav");
+  const bsCollapse = new bootstrap.Collapse(menuToggle, {
+    toggle: false,
+  });
+  navLinks.forEach((l) => {
+    l.addEventListener("click", () => {
+      bsCollapse.toggle();
+    });
+  });
+}
+
+// on scroll navbar big screen
 window.addEventListener("scroll", (e) => {
   const navbar = document.getElementById("navbar");
-  if (window.pageYOffset > 0) {
-    navbar.classList.add("add-shadow", "navbar-dark", "nav-color");
+
+  if (mediaQuery.matches) {
+    if (window.pageYOffset > 0) {
+      navbar.classList.add("add-shadow", "navbar-dark", "nav-color");
+    } else {
+      navbar.classList.remove("add-shadow");
+    }
   } else {
-    navbar.classList.remove("add-shadow", "navbar-dark", "nav-color");
+    if (window.pageYOffset > 0) {
+      navbar.classList.add("add-shadow", "navbar-dark", "nav-color");
+    } else {
+      navbar.classList.remove("add-shadow", "navbar-dark", "nav-color");
+    }
   }
 });
 
