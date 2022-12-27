cd2741e851df9e724255d1c8f42899cedbdc1167
prajotb <120003461+prajot-appsdeployer@users.noreply.github.com>
2022-12-27 15:21:29 +0000

    fix a bug causing the navbar flashing when clicked

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // on scroll effects
 const mediaQuery = window.matchMedia("(max-width: 990px)");
 if (mediaQuery.matches) {
-  window.onload = function () {
+  window.onload = function() {
     navbar.classList.add("navbar-dark", "nav-color");
   };
 
@@ -10,40 +10,45 @@ if (mediaQuery.matches) {
   const bsCollapse = new bootstrap.Collapse(menuToggle, {
     toggle: false,
   });
+
   navLinks.forEach((l) => {
     l.addEventListener("click", () => {
       bsCollapse.toggle();
     });
   });
+} else {
+
+  // on scroll navbar big screen
+  window.addEventListener("scroll", (e) => {
+    const navbar = document.getElementById("navbar");
+
+    if (mediaQuery.matches) {
+      if (window.pageYOffset > 0) {
+        navbar.classList.add("add-shadow", "navbar-dark", "nav-color");
+      } else {
+        navbar.classList.remove("add-shadow");
+      }
+    } else {
+      if (window.pageYOffset > 0) {
+        navbar.classList.add("add-shadow", "navbar-dark", "nav-color");
+      } else {
+        navbar.classList.remove("add-shadow", "navbar-dark", "nav-color");
+      }
+    }
+  });
+
+  const navbarLinks = document.querySelectorAll("#navbar a");
+  navbarLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      navbarLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      link.classList.add("active");
+    });
+  });
+
 }
 
-// on scroll navbar big screen
-window.addEventListener("scroll", (e) => {
-  const navbar = document.getElementById("navbar");
 
-  if (mediaQuery.matches) {
-    if (window.pageYOffset > 0) {
-      navbar.classList.add("add-shadow", "navbar-dark", "nav-color");
-    } else {
-      navbar.classList.remove("add-shadow");
-    }
-  } else {
-    if (window.pageYOffset > 0) {
-      navbar.classList.add("add-shadow", "navbar-dark", "nav-color");
-    } else {
-      navbar.classList.remove("add-shadow", "navbar-dark", "nav-color");
-    }
-  }
-});
 
-// Navbar active
-const navbarLinks = document.querySelectorAll("#navbar a");
-navbarLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    navbarLinks.forEach((link) => {
-      link.classList.remove("active");
-    });
-
-    link.classList.add("active");
-  });
-});
